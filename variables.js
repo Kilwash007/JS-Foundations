7cdbc26921fbba6cfaeac21217f5a548345c106b
Kilwanda Joshua <kilunda007@gmail.com>
2025-05-27 01:50:01 +0300

    add

---
+++ variables.js
@@ -0,0 +1,26 @@
+//1. Declaration
+//Declaring variables in JAVASCRIPT
+//var
+//let
+//const
+
+// a. Variable
+var variableName;
+console.log(variableName);
+
+variableName = "Hey There";
+console.log(variableName);
+
+//b. let
+let variableName1;
+console.log(variableName1);
+
+variableName1 = "Let variable";
+console.log(variableName1);
+
+//c. const
+const variableName2 = "Hello, world!";
+console.log(variableName2);
+
+//2. Assignment
+//3. Initialization
